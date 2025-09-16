75a086ecf0eb8f3ec47c81c73cf74aff089413d0
Mirsait <cabmupcaut@gmail.com>
2025-09-16 09:11:37 -0700

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task1{
+		public static void main(String[] args){
+			Scanner scanner = new Scanner(System.in);
+			float a = scanner.nextFloat();
+			float b = scanner.nextFloat();
+			System.out.println(a + b);
+		}
+
+	}
