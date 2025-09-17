e81a5b2dba413a136fb209018e6fa2859ab6f54f
Mirsait <cabmupcaut@gmail.com>
2025-09-16 22:52:26 -0700

    Update Task2_1.java

--- Task2_1.java
+++ Task2_1.java
@@ -5,16 +5,16 @@ public class Task2_1{
 		Scanner scanner = new Scanner(System.in);
 		int m = scanner.nextInt();
 		int n = scanner.nextInt();
-		for (int i = 0; i <= m - 1; i++){
+		for (int i = 1; i <= m; i++){
 			System.out.print("1");
 		} System.out.println();
-		for (int z = 0; z <= n - 3; z++){
+		for (int z = 1; z <= n - 2; z++){
 			System.out.print("1");
-			for (int y = 0; y <= m - 3; y++){
+			for (int y = 1; y <= m - 2; y++){
 				System.out.print(" ");
 			}System.out.println("1");
 		}
-		for (int i = 0; i <= m - 1; i++){
+		for (int i = 1; i <= m; i++){
 			System.out.print("1");
 		}System.out.println();
 	}
