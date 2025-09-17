4fe4134c00abfc47377df2392ac6fb16125cde78
Mirsait <cabmupcaut@gmail.com>
2025-09-17 07:31:04 -0700

    Update Task2_1.java

--- Task2_1.java
+++ Task2_1.java
@@ -8,14 +8,21 @@ public class Task2_1{
 		for (int i = 1; i <= m; i++){
 			System.out.print("1");
 		} System.out.println();
-		for (int z = 1; z <= n - 2; z++){
-			System.out.print("1");
-			for (int y = 1; y <= m - 2; y++){
-				System.out.print(" ");
-			}System.out.println("1");
-		}
-		for (int i = 1; i <= m; i++){
-			System.out.print("1");
-		}System.out.println();
+		
+		for (int i = 1; i <= n - 2; i++) {
+            System.out.print("1");   
+            for (int j = 1; j <= m - 2; j++){
+            	System.out.print(" ");
+            }
+            if (m > 1) System.out.print("1");   
+            System.out.println();
+        }
+		
+		if (n > 1) {                   
+            for (int i = 1; i <= m; i++) {
+            	System.out.print("1");
+        }
+            System.out.println();
+        }
 	}
 }
