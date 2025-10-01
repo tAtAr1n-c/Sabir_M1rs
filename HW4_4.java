8edc20ea9aa5bb79c9527444b0f1d90486dc7122
Mirsait <cabmupcaut@gmail.com>
2025-10-01 14:26:01 -0700

    Create HW4_4.java

---
+++ HW4_4.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int str = scanner.nextInt();
+        int stol = scanner.nextInt();
+        int[][] array = new int[str][stol];
+        int cou = 0;
+        int finco = 0;
+
+        for(int i = 0; i < str; i++){
+            for(int j = 0; j < stol; j++){
+                System.out.println("Введи число для индекса [" + i + "] [" + j + "]");
+                array[i][j] = scanner.nextInt();
+                if(Podryad(array[i][j]) == true){
+                    cou++;
+                }
+            }
+        }
+        if (cou == str * 2){
+            System.out.println("DA VO VSEH");
+        }else{
+            System.out.println("NET NE VO VSEH");
+        }
+        for(int i = 0; i < str; i++){
+            for(int j = 0; j < stol; j++){
+                System.out.print(array[i][j] + " ");
+            }System.out.println();
+        }
+    }
+
+    public static boolean Podryad(int x){
+        while (x >= 10) {
+            int  slev= x % 10;
+            int  sprav= (x / 10) % 10;
+            if (sprav >= slev) {
+                return false;
+            }
+            x /= 10;
+        }
+        return true;
+    }
+}
