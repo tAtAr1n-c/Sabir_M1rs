3fa4d76108d1ed02d4e2123929cf70bc3b0c408b
Mirsait <cabmupcaut@gmail.com>
2025-10-11 19:43:54 +0300

    Create HW5_3.java

---
+++ HW5_3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int str = scanner.nextInt();
+        int stol = scanner.nextInt();
+        int c = 0;
+        int finco = 0;
+        String[][] array = new String[str][stol];
+        for(int i = 0; i < str; i++){
+            for(int j = 0; j < stol; j++){
+                System.out.print("Введите слово с индесом [" + i + "] [" + j +  "] : ");
+                array[i][j] = scanner.next();
+                if(Poslglas(array[i][j])){
+                    c++;
+                }
+            }
+            if(c >= 1){
+                finco++;
+                c = 0;
+            }
+        }
+        if(finco == str){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        
+        for(int i = 0; i < str; i++){
+            for(int j = 0; j < stol; j++){
+                System.out.print(array[i][j] + " ");
+            } System.out.println();
+        }
+     }
+    public static boolean Poslglas(String x){
+        String bukvi = "уеёыаоэяиюУЕЁЫАОЭЯИЮ";
+        if(bukvi.contains(x.charAt(x.length() - 1) + "")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
