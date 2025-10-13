7e3ef3fbdcfd7ffa2fa8f5782ccc4aeb621daa04
Mirsait <cabmupcaut@gmail.com>
2025-10-13 14:13:19 +0300

    Update HW5_1.java

--- HW5_1.java
+++ HW5_1.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args){
-        boolean flag = true;
-        Scanner scanner = new Scanner(System.in);
-        String bukvi = scanner.nextLine().replace(" ", "").toLowerCase().replace(",", "").replace(".", "");
-        for(int i = 0; i < bukvi.length(); i++){
-            if(bukvi.charAt(i) != bukvi.charAt(bukvi.length() - 1 - i)){
-                flag = false;
+        Scanner enter = new Scanner(System.in);
+        String start = enter.nextLine();
+        String konec = "";
+        for (int i = 0 ; i < start.length() ; i++){
+            char bukva = start.charAt(i);
+            if (bukva != ' ' && bukva != '.' && bukva != ',' && bukva != ':'){
+                konec = konec + start.charAt(i);
             }
         }
-        if (flag == true){
+        int count = 0;
+        for (int i = 0 ; i < konec.length() ; i++){
+            if (ToLower(konec.charAt(i)) != ToLower(konec.charAt(konec.length() - 1 - i))){
+                count++;
+            }
+        }
+        System.out.println(konec);
+        if (count == 0){
             System.out.println(true);
         }else{
             System.out.println(false);
+        }
+    }
+    public static char ToLower(char x) {
+        if ('A' <= x && x <= 'Z') {
+            int y = x + 32;
+            return (char) y;
+        }else{
+            return x;
         }
     }
 }
