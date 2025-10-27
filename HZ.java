4ea10c34a7cf7077a959b3dcc87bc57bf2402743
Mirsait <cabmupcaut@gmail.com>
2025-10-27 14:38:00 +0300

    Update HZ.java

--- HZ.java
+++ HZ.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите свое имя: ");
-        String str = scanner.nextLine();
-        if(str.matches("[A-Z][a-z]*|[А-Я][а-я]*}")) System.out.println(str);
-        else System.out.println("ВВеди имя верно");
+        boolean flag = true;
+        while(flag){
+            System.out.print("Введите свое имя: ");
+            String str = scanner.nextLine();
+            if(str.matches("[A-Z][a-z]{1,14}|[А-Я][а-я]{1,14}")){ System.out.println(str); flag = false;}
+            else System.out.println("ВВеди имя верно, ты ввел неверно.");
+        }
     }
 }
